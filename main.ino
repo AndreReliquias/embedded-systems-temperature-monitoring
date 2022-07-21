4351da21729e935746ddaaddb66b636d8d665a6d
AndreReliquias <andrereliquias.contato@gmail.com>
2022-07-21 10:38:21 -0400

    final code

--- main.ino
+++ main.ino
@@ -1,15 +1,43 @@
 #include "DHT.h"
-DHT dht2(D1, DHT11);
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
-bool s = false;
+#define DHT_PIN D1
+#define DHT_TYPE DHT11
+#define COOLER_PIN D3
+
+DHT dht2(DHT_PIN, DHT_TYPE);
+
+bool coolerIsDisable = false;
 float temperature = 0;
+const char *ssid = "ssid-here";
+const char *password = "pw-here";
+String host = "host-here";
+const char *fingerprint = "fingerprint-here";
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(D1, INPUT);
-  pinMode(D3, OUTPUT);
+  pinMode(DHT_PIN, INPUT);
+  pinMode(COOLER_PIN, OUTPUT);
   dht2.begin();
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
@@ -17,17 +45,53 @@ void loop()
   temperature = dht2.readTemperature();
   Serial.println("Temperature in C:");
   Serial.println(temperature);
-  Serial.println(s);
+  Serial.println("Is the cooler disable?");
+  Serial.println(coolerIsDisable);
 
-  if (temperature > 22)
+  if ((float)temperature > 22)
   {
-    s = true;
+    coolerIsDisable = false;
   }
-  else if (temperature < 22 && s)
+  else if ((float)temperature < 22)
   {
-    s = false;
+    coolerIsDisable = true;
   }
 
-  digitalWrite(D3, s);
+  digitalWrite(COOLER_PIN, coolerIsDisable);
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    BearSSL::WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient http;
+
+    String serverPath = "/prod/v1/datacenterlogs?temperature=" + (String)temperature;
+    String url = serverPath.c_str();
+
+    http.begin(client, host, 443, serverPath, fingerprint);
+
+    int httpResponseCode = http.POST("");
+
+    if (httpResponseCode > 0)
+    {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    else
+    {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+
+    http.end();
+  }
+  else
+  {
+    Serial.println("WiFi Disconnected");
+  }
+
   delay(1000);
 }
